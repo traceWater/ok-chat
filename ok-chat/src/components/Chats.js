ada6b7404c65cea715350466e90965bf825f8a97
traceWater <100964288+traceWater@users.noreply.github.com>
2022-06-16 11:52:10 -0700

    Add files via upload

---
+++ ok-chat/src/components/Chats.js
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+import { ChatEngine } from "react-chat-engine";
+import { auth } from "../firebase";
+import { useAuth } from "../contexts/AuthContext";
+import axios from "axios";
+
+const Chats = () => {
+  const history = useHistory();
+  const { user } = useAuth();
+  const [loading, setLoading] = useState(true);
+
+  const handleLogout = async () => {
+    await auth.signOut();
+    history.push("/");
+  };
+
+  const getFile = async (url) => {
+    let response = await fetch(url);
+    let data = await response.blob();
+    return new File([data], "userPhoto.jpg", { type: "image/jpeg" });
+  };
+
+  useEffect(() => {
+    if (!user) {
+      history.push("/");
+      return;
+    }
+    axios.get(
+        'https://api.chatengine.io/users/me/',
+        { headers: { 
+          "project-id": "69ebb65c-05cc-4176-b0c9-30285d4e578e",
+          "user-name": user.email,
+          "user-secret": user.uid
+        }}
+      )
+      .then(() => {
+        setLoading(false);
+      })
+      .catch(() => {
+        let formdata = new FormData();
+        formdata.append("email", user.email);
+        formdata.append("username", user.email);
+        formdata.append("secret", user.uid);
+
+        getFile(user.photoURL)
+        .then(avatar => {
+          formdata.append('avatar', avatar, avatar.name)
+
+          axios.post(
+            'https://api.chatengine.io/users/',
+            formdata,
+            { headers: { "private-key": process.env.REACT_APP_CHAT_ENGINE_KEY }}
+          )
+          .then(() => setLoading(false))
+          .catch(e => console.log(e.response))
+        })
+      });
+  }, [user, history]);
+
+  if (!user || loading) return <div id="loading-container">Loading</div>;
+
+  return (
+    <div className="chats-page">
+      <div className="nav-bar">
+        <div className="logo-tab">ChatApp</div>
+        <div className="logout-tab" onClick={handleLogout}>
+          Logout
+        </div>
+      </div>
+      <ChatEngine
+        height="calc(100vh - 66px)"
+        projectID={process.env.REACT_APP_CHAT_ENGINE_ID}
+        userName={user.email}
+        userSecret={user.uid}
+      />
+    </div>
+  );
+};
+
+export default Chats;
---
+++ ok-chat/src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+export const auth = firebase
+  .initializeApp({
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+  })
+  .auth();
